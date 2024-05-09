5dfd70ba5f080709ff38520a16230aaa630431a4
Official Mark-HDR <128824042+Mark-HDR@users.noreply.github.com>
2024-05-10 01:29:31 +0700

    Update add-vless.sh

--- xray/add-vless.sh
+++ xray/add-vless.sh
@@ -38,16 +38,29 @@ sed -i '/#vless$/a\#& '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
 sed -i '/#vlessgrpc$/a\#& '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
+
+# Getting IP information
+ip_info=$(curl -sS https://ipinfo.io/json)
+ip=$(echo $ip_info | jq -r '.ip')
+country=$(echo $ip_info | jq -r '.country')
+region=$(echo $ip_info | jq -r '.region')
+isp=$(echo $ip_info | jq -r '.org')
+
 vlesslink1="vless://${uuid}@${domain}:$tls?path=/vless&security=tls&encryption=none&type=ws#${user}"
 vlesslink2="vless://${uuid}@${domain}:$none?path=/vless&encryption=none&type=ws#${user}"
 vlesslink3="vless://${uuid}@${domain}:$tls?mode=gun&security=tls&encryption=none&type=grpc&serviceName=vless-grpc&sni=bug.com#${user}"
 systemctl restart xray
+
 clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vless.log
 echo -e "\E[44;1;39m        Vless Account        \E[0m" | tee -a /etc/log-create-vless.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vless.log
 echo -e "Remarks        : ${user}" | tee -a /etc/log-create-vless.log
 echo -e "Domain         : ${domain}" | tee -a /etc/log-create-vless.log
+echo -e "IP             : ${ip}" | tee -a /etc/log-create-vless.log
+echo -e "Country        : ${country}" | tee -a /etc/log-create-vless.log
+echo -e "Region         : ${region}" | tee -a /etc/log-create-vless.log
+echo -e "ISP            : ${isp}" | tee -a /etc/log-create-vless.log
 echo -e "Wildcard       : (bug.com).${domain}" | tee -a /etc/log-create-vless.log
 echo -e "Port TLS       : $tls" | tee -a /etc/log-create-vless.log
 echo -e "Port none TLS  : $none" | tee -a /etc/log-create-vless.log
