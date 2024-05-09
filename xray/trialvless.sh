ed58fab0b2d53013e5e293a891f3994210dbc1a1
Official Mark-HDR <128824042+Mark-HDR@users.noreply.github.com>
2024-05-10 01:37:10 +0700

    Update trialvless.sh

--- xray/trialvless.sh
+++ xray/trialvless.sh
@@ -9,34 +9,46 @@ sed -i '/#vless$/a\#& '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
 sed -i '/#vlessgrpc$/a\#& '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
+# Getting IP information
+ip_info=$(curl -sS https://ipinfo.io/json)
+ip=$(echo $ip_info | jq -r '.ip')
+country=$(echo $ip_info | jq -r '.country')
+region=$(echo $ip_info | jq -r '.region')
+isp=$(echo $ip_info | jq -r '.org')
+
 vlesslink1="vless://${uuid}@${domain}:$tls?path=/vless&security=tls&encryption=none&type=ws#${user}"
 vlesslink2="vless://${uuid}@${domain}:$none?path=/vless&encryption=none&type=ws#${user}"
 vlesslink3="vless://${uuid}@${domain}:$tls?mode=gun&security=tls&encryption=none&type=grpc&serviceName=vless-grpc&sni=bug.com#${user}"
 systemctl restart xray
+
 clear
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[44;1;39m        Trial Vless        \E[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Remarks        : ${user}"
-echo -e "Domain         : ${domain}"
-echo -e "Wildcard       : (bug.com).${domain}"
-echo -e "Port TLS       : $tls"
-echo -e "Port none TLS  : $none"
-echo -e "Port gRPC      : $tls"
-echo -e "ID             : ${uuid}"
-echo -e "Encryption     : none"
-echo -e "Network        : ws"
-echo -e "Path           : /vless"
-echo -e "Path           : vless-grpc"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link TLS       : ${vlesslink1}"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link none TLS  : ${vlesslink2}"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link gRPC      : ${vlesslink3}"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Expired On     : $exp"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vless.log
+echo -e "\E[44;1;39m        Vless Account        \E[0m" | tee -a /etc/log-create-vless.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vless.log
+echo -e "Remarks        : ${user}" | tee -a /etc/log-create-vless.log
+echo -e "Domain         : ${domain}" | tee -a /etc/log-create-vless.log
+echo -e "IP             : ${ip}" | tee -a /etc/log-create-vless.log
+echo -e "Country        : ${country}" | tee -a /etc/log-create-vless.log
+echo -e "Region         : ${region}" | tee -a /etc/log-create-vless.log
+echo -e "ISP            : ${isp}" | tee -a /etc/log-create-vless.log
+echo -e "Wildcard       : (bug.com).${domain}" | tee -a /etc/log-create-vless.log
+echo -e "Port TLS       : $tls" | tee -a /etc/log-create-vless.log
+echo -e "Port none TLS  : $none" | tee -a /etc/log-create-vless.log
+echo -e "id             : ${uuid}" | tee -a /etc/log-create-vless.log
+echo -e "Encryption     : none" | tee -a /etc/log-create-vless.log
+echo -e "Network        : ws" | tee -a /etc/log-create-vless.log
+echo -e "Path           : /vless" | tee -a /etc/log-create-vless.log
+echo -e "Path           : vless-grpc" | tee -a /etc/log-create-vless.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vless.log
+echo -e "Link TLS       : ${vlesslink1}" | tee -a /etc/log-create-vless.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vless.log
+echo -e "Link none TLS  : ${vlesslink2}" | tee -a /etc/log-create-vless.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vless.log
+echo -e "Link gRPC      : ${vlesslink3}" | tee -a /etc/log-create-vless.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vless.log
+echo -e "Expired On     : $exp" | tee -a /etc/log-create-vless.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-vless.log
+echo "" | tee -a /etc/log-create-vless.log
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 m-vless
