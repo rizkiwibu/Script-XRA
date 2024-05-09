d6f54c9839869fbe5039066e97a19ed3aaf89901
Official Mark-HDR <128824042+Mark-HDR@users.noreply.github.com>
2024-05-10 01:36:27 +0700

    Update trialtrojan.sh

--- xray/trialtrojan.sh
+++ xray/trialtrojan.sh
@@ -10,32 +10,44 @@ sed -i '/#trojanws$/a\#! '"$user $exp"'\
 sed -i '/#trojangrpc$/a\#! '"$user $exp"'\
 },{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
 
+# Getting IP information
+ip_info=$(curl -sS https://ipinfo.io/json)
+ip=$(echo $ip_info | jq -r '.ip')
+country=$(echo $ip_info | jq -r '.country')
+region=$(echo $ip_info | jq -r '.region')
+isp=$(echo $ip_info | jq -r '.org')
+
 systemctl restart xray
 trojanlink1="trojan://${uuid}@${domain}:${tls}?mode=gun&security=tls&type=grpc&serviceName=trojan-grpc&sni=bug.com#${user}"
-trojanlink="trojan://${uuid}@isi_bug_disini:${tls}?path=%2Ftrojan-ws&security=tls&host=${domain}&type=ws&sni=${domain}#${user}"
-trojanlink2="trojan://${uuid}@isi_bug_disini:${ntls}?path=%2Ftrojan-ws&security=tls&host=${domain}&type=ws#${user}"
+trojanlink="trojan://${uuid}@${domain}:${tls}?path=%2Ftrojan-ws&security=tls&host=${domain}&type=ws&sni=isi_bug_disini#${user}"
+trojanlink2="trojan://${uuid}@isi_bug_disini:${ntls}?path=%2Ftrojan-ws&security=none&host=${domain}&type=ws#${user}"
 clear
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[0;41;36m           TRIAL TROJAN           \E[0m"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Remarks        : ${user}"
-echo -e "Host/IP        : ${domain}"
-echo -e "Wildcard       : (bug.com).${domain}"
-echo -e "Port TLS       : ${tls}"
-echo -e "Port none TLS  : ${ntls}"
-echo -e "Port gRPC      : ${tls}"
-echo -e "Key            : ${uuid}"
-echo -e "Path           : /trojan-ws"
-echo -e "ServiceName    : trojan-grpc"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link TLS       : ${trojanlink}"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link none TLS  : ${trojanlink2}"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Link gRPC      : ${trojanlink1}"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "Expired On     : $exp"
-echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-trojan.log
+echo -e "\E[0;41;36m           TROJAN ACCOUNT           \E[0m" | tee -a /etc/log-create-trojan.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-trojan.log
+echo -e "Remarks        : ${user}" | tee -a /etc/log-create-trojan.log
+echo -e "Host/IP        : ${domain}" | tee -a /etc/log-create-trojan.log
+echo -e "IP             : ${ip}" | tee -a /etc/log-create-trojan.log
+echo -e "Country        : ${country}" | tee -a /etc/log-create-trojan.log
+echo -e "Region         : ${region}" | tee -a /etc/log-create-trojan.log
+echo -e "ISP            : ${isp}" | tee -a /etc/log-create-trojan.log
+echo -e "Wildcard       : (bug.com).${domain}" | tee -a /etc/log-create-trojan.log
+echo -e "Port TLS       : ${tls}" | tee -a /etc/log-create-trojan.log
+echo -e "Port none TLS  : ${ntls}" | tee -a /etc/log-create-trojan.log
+echo -e "Port gRPC      : ${tls}" | tee -a /etc/log-create-trojan.log
+echo -e "Key            : ${uuid}" | tee -a /etc/log-create-trojan.log
+echo -e "Path           : /trojan-ws" | tee -a /etc/log-create-trojan.log
+echo -e "ServiceName    : trojan-grpc" | tee -a /etc/log-create-trojan.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-trojan.log
+echo -e "Link TLS       : ${trojanlink}" | tee -a /etc/log-create-trojan.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-trojan.log
+echo -e "Link none TLS  : ${trojanlink2}" | tee -a /etc/log-create-trojan.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-trojan.log
+echo -e "Link gRPC      : ${trojanlink1}" | tee -a /etc/log-create-trojan.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-trojan.log
+echo -e "Expired On     : $exp" | tee -a /etc/log-create-trojan.log
+echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-trojan.log
+echo "" | tee -a /etc/log-create-trojan.log
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu"
 
