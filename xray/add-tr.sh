f2f16c84932ffe32a28d60b17571c6b2926d3980
Official Mark-HDR <128824042+Mark-HDR@users.noreply.github.com>
2024-05-10 01:27:40 +0700

    Update add-tr.sh

--- xray/add-tr.sh
+++ xray/add-tr.sh
@@ -40,6 +40,13 @@ sed -i '/#trojanws$/a\#! '"$user $exp"'\
 sed -i '/#trojangrpc$/a\#! '"$user $exp"'\
 },{"password": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
 
+# Getting IP information
+ip_info=$(curl -sS https://ipinfo.io/json)
+ip=$(echo $ip_info | jq -r '.ip')
+country=$(echo $ip_info | jq -r '.country')
+region=$(echo $ip_info | jq -r '.region')
+isp=$(echo $ip_info | jq -r '.org')
+
 trojanlink1="trojan://${uuid}@${domain}:${tls}?mode=gun&security=tls&type=grpc&serviceName=trojan-grpc&sni=bug.com#${user}"
 trojanlink="trojan://${uuid}@${domain}:${tls}?path=%2Ftrojan-ws&security=tls&host=${domain}&type=ws&sni=isi_bug_disini#${user}"
 trojanlink2="trojan://${uuid}@isi_bug_disini:${ntls}?path=%2Ftrojan-ws&security=none&host=${domain}&type=ws#${user}"
@@ -49,6 +56,10 @@ echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━�
 echo -e "\E[0;41;36m           TROJAN ACCOUNT           \E[0m" | tee -a /etc/log-create-trojan.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-trojan.log
 echo -e "Remarks        : ${user}" | tee -a /etc/log-create-trojan.log
+echo -e "IP             : ${ip}" | tee -a /etc/log-create-trojan.log
+echo -e "Country        : ${country}" | tee -a /etc/log-create-trojan.log
+echo -e "Region         : ${region}" | tee -a /etc/log-create-trojan.log
+echo -e "ISP            : ${isp}" | tee -a /etc/log-create-trojan.log
 echo -e "Host/IP        : ${domain}" | tee -a /etc/log-create-trojan.log
 echo -e "Wildcard       : (bug.com).${domain}" | tee -a /etc/log-create-trojan.log
 echo -e "Port TLS       : ${tls}" | tee -a /etc/log-create-trojan.log
