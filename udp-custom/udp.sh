8049a5119785fc84f9d6f4db29fe633b4e1c04a5
Official Mark-HDR <128824042+Mark-HDR@users.noreply.github.com>
2024-05-10 00:56:08 +0700

    Update udp.sh

--- udp-custom/udp.sh
+++ udp-custom/udp.sh
@@ -23,13 +23,13 @@ rm -rf /usr/bin/trial
 rm -rf /root/domain
 echo "\e[1;32m Update Menu.. \e[0m"
 sleep 1
-wget -q -O /usr/bin/usernew https://raw.githubusercontent.com/caliphdev/AutoScriptXray/master/udp-custom/ssh/usernew.sh
-wget -q -O /usr/bin/trial https://raw.githubusercontent.com/caliphdev/AutoScriptXray/master/udp-custom/ssh/trial.sh
+wget -q -O /usr/bin/usernew https://raw.githubusercontent.com/Mark-HDR/Script-XRAY/master/udp-custom/ssh/usernew.sh
+wget -q -O /usr/bin/trial https://raw.githubusercontent.com/Mark-HDR/Script-XRAY/master/udp-custom/ssh/trial.sh
 echo "\e[1;32m Proses Download Script Slowdns.. \e[0m"
-wget https://raw.githubusercontent.com/caliphdev/AutoScriptXray/master/udp-custom/slowdns/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
+wget https://raw.githubusercontent.com/Mark-HDR/Script-XRAY/master/udp-custom/slowdns/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
 sleep 1
 echo "\e[1;32m Proses Download Script OpenVPN.. \e[0m"
-wget https://raw.githubusercontent.com/caliphdev/AutoScriptXray/master/udp-custom/openvpn/openvpn.sh && chmod +x openvpn.sh && ./openvpn.sh
+wget https://raw.githubusercontent.com/Mark-HDR/Script-XRAY/master/udp-custom/openvpn/openvpn.sh && chmod +x openvpn.sh && ./openvpn.sh
 sleep 1
 chmod +x /usr/bin/usernew
 chmod +x /usr/bin/trial
